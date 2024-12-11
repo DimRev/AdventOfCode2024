e82af53e699fd54870af9a3e2b9d7be4de6136ae
DimRev <dimarev444@gmail.com>
2024-12-11 08:22:19 +0200

    Add solutions for Python, d11, p1

---
+++ Python/d11/p1.py
@@ -0,0 +1,36 @@
+def parse_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            yield line.strip()
+
+
+def apply_rules(line_list):
+    new_line_list = []
+    for i, num in enumerate(line_list):
+        if num == 0:
+            new_line_list.append(1)
+        elif len(str(num)) > 1 and len(str(num)) % 2 == 0:
+            num_str = str(num)
+            m = len(num_str) // 2
+            new_line_list.append(int(num_str[:m]))
+            new_line_list.append(int(num_str[m:]))
+        else:
+            new_line_list.append(num * 2024)
+    return new_line_list
+
+
+def solve(path, n):
+    for line in parse_file(path):
+        line_list = list(map(lambda x: int(x), line.split()))
+        for i in range(n):
+            line_list = apply_rules(line_list)
+        print(len(line_list))
+
+
+def main():
+    # solve("Python/d11/small.txt", 6)
+    solve("Python/d11/input.txt", 25)
+
+
+if __name__ == "__main__":
+    main()
